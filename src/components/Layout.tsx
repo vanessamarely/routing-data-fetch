230035bd94bf52e6015788b1c63c9cb2924bf9d1
vanessamarely <vanessamarely@gmail.com>
2023-04-13 20:42:51 -0500

    main on layout removed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+     {children}
     </>
   );
 }
--- src/pages/postsFetch/[id].tsx
+++ src/pages/postsFetch/[id].tsx
@@ -2,6 +2,7 @@ import { getPostDetails, getPostIdList } from "../../lib/post";
 import { Data, Params } from "./type";
 
 export default function Post({ postData }: { postData: Data }) {
+  console.log(postData)
   return (
     <div className="bg-gray-800 p-16 text-gray-100">
       <div className="text-center font-bold text-3xl">{postData.title}</div>
@@ -22,7 +23,9 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }: Params ) {
+  console.log(params)
   const postData = await getPostDetails(params.id);
+  console.log(postData)
   return {
     props: {
       postData,
